1db9f8dcb86acb6bbead5defcaa2146d15b0e2ab
Aayush Rijal <arijal@aiims.com.au>
2023-04-12 09:59:06 +1000

    Form updated with webhook

--- app/src/form.php
+++ app/src/form.php
@@ -48,6 +48,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['token'])) {
                     </head>
                 <body><table><tbody>' .
             '<tr>' .
+            '<td>Page Reference</td>' .
+            '<td><b>' . $pageRef . '</b></td>' .
+            '</tr>' .
+            '<tr>' .
             '<td>Company Name or ABN</td>' .
             '<td><b>' . strip_tags($abn) . '</b></td>' .
             '</tr>' .
@@ -73,16 +77,49 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['token'])) {
             '</tr>' .
             '</tbody></table></body></html>';
 
+        // $webhook_url = 'https://hooks.zapier.com/hooks/catch/4537599/32ezrf1/';
+
+        $webhook_url = 'https://hooks.zapier.com/hooks/catch/14919517/32exebk/';
+
+        $form_data = array(
+            'abn' => strip_tags($abn),
+            'fullName' => strip_tags($fullName),
+            'phoneNumber' => strip_tags($phoneNumber),
+            'email' => strip_tags($email),
+            'loanAmount' => strip_tags($loanAmount),
+            'foundVehicle' => strip_tags($foundVehicle),
+            'pageRef' => $pageRef
+        );
+
+        $json_data = json_encode($form_data);
+
+        $curl_options = array(
+            CURLOPT_URL => $webhook_url,
+            CURLOPT_POST => 1,
+            CURLOPT_POSTFIELDS => $json_data,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($json_data)
+            )
+        );
+
+        // Initialize cURL and send the request
+        $ch = curl_init();
+        curl_setopt_array($ch, $curl_options);
+        $response = curl_exec($ch);
+        curl_close($ch);
+
         $headers = "MIME-Version: 1.0\r\n" .
             "Content-type: text/html; charset=utf-8\r\n" .
             "From: " . $site . " <" . $no_reply_email . ">" . "\r\n" .
             // "Bcc: " . $bcc_email . "\r\n" .
             "Reply-To: " . $site . " <" . $email . ">" . "\r\n" .
             "X-Mailer: PHP/" . phpversion();
+
         $result = mail($to, $subject, $message, $headers);
 
-        if ($result) {
-            header('location:./../thankyou');
+        if ($result || $response === 'success') {
+            header('Location: ./../thankyou.php');
         } else {
             throw new Exception('Failed, please submit form again or call us directly.');
         }
